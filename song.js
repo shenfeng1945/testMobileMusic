06f0cba12e09a730b33fcb2fe0d7fadaca64b695
shenfeng1945 <823902826@qq.com>
2017-08-26 12:36:53 +0800

    bug

--- song.js
+++ song.js
@@ -3,7 +3,7 @@
  */
 
 let audio = document.createElement('audio')
-audio.src = 'http://ov4sliu3n.bkt.clouddn.com/955d%252F222a%252F3b98%252F1115a4c61c8e7fc1c65a96c8c0efd5db.mp3'
+audio.src = '//ov4sliu3n.bkt.clouddn.com/955d%252F222a%252F3b98%252F1115a4c61c8e7fc1c65a96c8c0efd5db.mp3'
 audio.oncanplay = function () {
     audio.play()
 }
