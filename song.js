8ede80e91cbc10ef9af81c6296ba8bd88d235bda
shenfeng1945 <823902826@qq.com>
2017-08-26 12:42:49 +0800

    15

--- song.js
+++ song.js
@@ -4,9 +4,12 @@
 
 let audio = document.createElement('audio')
 audio.src = '//ov4sliu3n.bkt.clouddn.com/955d%252F222a%252F3b98%252F1115a4c61c8e7fc1c65a96c8c0efd5db.mp3'
-audio.oncanplay = function () {
+audio.addEventListener('canplay',function(){
     audio.play()
-}
+})
+// audio.oncanplay = function () {
+//     audio.play()
+// }
 $('.icon-pause').on('click', function () {
     audio.pause()
     $('.disc-container').removeClass('playing')
