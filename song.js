31dad865918ff8d7a4b902c53724b6d5e49bd9a4
shenfeng1945 <823902826@qq.com>
2017-08-28 00:13:17 +0800

    通过加查询参数，解决了同源下不同的歌曲播放

--- song.js
+++ song.js
@@ -3,41 +3,107 @@
  */
 
 let audio = document.createElement('audio')
-audio.src = '//ov4sliu3n.bkt.clouddn.com/955d%252F222a%252F3b98%252F1115a4c61c8e7fc1c65a96c8c0efd5db.mp3'
-audio.addEventListener('canplay',function(){
+// audio.src = "http://ov4sliu3n.bkt.clouddn.com/%E6%88%91%E7%9A%84%E4%B8%80%E4%B8%AA%E9%81%93%E5%A7%91%E6%9C%8B%E5%8F%8B.mp3"
+audio.oncanplay = function () {
     audio.play()
-})
-// audio.oncanplay = function () {
-//     audio.play()
-// }
+}
 $('.icon-pause').on('click', function () {
     audio.pause()
-    $('.disc').addClass('no-animation')
     $('.disc-container').removeClass('playing')
 })
 $('.icon-play').on('click', function () {
     audio.play()
-    $('.disc').removeClass('no-animation')
     $('.disc-container').addClass('playing')
 })
-
-// $(function () {
-//     $.get('/song.json').then(function (object) {
-//         let { lyric } = object
+$(function(){
+    let url=window.location.href
+    let reg = /.*\=(.*)/
+    let id = url.match(reg)[1]
+    $.get('./song.json').then(function(object){
+        for(var i=0;i<object.length;i++){
+            if(object[i].id === id){
+                console.log(object[i]['img'])
+            //    xx(object[i].name,object[i].lyric,object[i].singer,object[i]['img'],object[i]['song'])
+               xx({
+                   name: object[i].name,
+                   lyric: object[i]['lyric'],
+                   singer: object[i]['singer'],
+                   song: object[i]['song'],
+                   img: object[i]['img'],
+                   background: object[i]['background']
+               })
+            }
+        }
+    })
+    function xx(options){
+        let {name,lyric,singer,song,background,img} = options
+        console.log(name,singer,background)
+        let image = document.querySelector('.center-image') 
+        let $page = $('.page')
+        $page.css('background',`transparent url(${background}) no-repeat`)
+       $('.lyric-head').text(`${name} - ${singer}`)
+       image.src = img
+       //想听什么歌
+       audio.src = song
+       let array = lyric.split('\n')
+       let reg = /\[(.*)\](.+)/
+       let arr = []
+       let $lyric = $('.lyric-moving')
+       array = array.map((string) => {
+           let obj = {}
+           obj = string.match(reg)
+           return obj
+       })
+       for(var i=0;i<array.length;i++){
+           if(array[i]){
+               let $p = `
+                  <p data-time="${array[i][1]}">${array[i][2]}</p>
+                    `
+               $lyric.append($p)
+           }
+       }
+    }
+})
+// $(function(){
+//     $.get('./song.json').then(function(object){
+//         let {lyric} = object
 //         let array = lyric.split('\n')
-//         let reg = /\[(.+)\](.*)/
+//         let reg = /\[(.*)\](.+)/
 //         let arr = []
 //         let $lyric = $('.lyric-moving')
-//         for (let i = 0; i < array.length; i++) {
-//             if (array[i]) {
-//                 arr.push(array[i].match(reg))
+//         array = array.map((string) => {
+//             let obj = {}
+//             obj = string.match(reg)
+//             return obj
+//         })
+//         for(var i=0;i<array.length;i++){
+//             if(array[i]){
+//                 let $p = `
+//                    <p data-time="${array[i][1]}">${array[i][2]}</p>
+//                      `
+//                 $lyric.append($p)
 //             }
-//             if(arr[i][2]){
-//             var $p = $('<p>')
-//             $p.attr('date-time', arr[i][1]).text(arr[i][2])
-//             $p.appendTo($lyric)
-//             }
-//         console.log(arr)
 //         }
 //     })
 // })
+setInterval(function(){
+     let time = audio.currentTime
+     let minute = ~~(time/60)
+     let newminute = (minute>9)?minute:'0'+minute
+     let second = time - minute*60
+     let newSecond = (second > 10)?second:`0`+second
+    // console.log(newSecond)
+     let newTime = `${newminute}:${newSecond}`
+     let $p = $('.lyric-moving>p')
+    $.each($p,function(item){
+        let pTime = $p.eq(item).attr('data-time')
+        let pNextTime = $p.eq(item+1).attr('data-time')
+        // console.log(pTime,pNextTime)
+        if(newTime >= pTime && newTime < pNextTime){
+            let movingPx = `-${(item-1)*24}px`
+            $('.lyric-moving').css('transform',`translateY(${movingPx})`)
+            $p.eq(item).css('color','white')
+            $p.eq(item-1).css('color','rgba(255,255,255,.6)')
+        }
+    })
+},300)
